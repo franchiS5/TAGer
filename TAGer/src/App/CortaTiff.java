0c14765c29cbf47ad2b6d30891cca9012d8663c0
Luis <Luis@192.168.1.36>
2015-01-18 17:51:32 +0100

    18/01/2015. Last Update. TIFF Write OK. Jhove Validation OK. Metadata from image_input copy to image_output. Working Hard.

--- TAGer/src/App/CortaTiff.java
+++ TAGer/src/App/CortaTiff.java
@@ -1,32 +1,34 @@
 package App;
 
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.RandomAccessFile;
+import java.nio.ByteOrder;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Locale;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.spi.ImageWriterSpi;
+import javax.imageio.stream.ImageInputStream;
 import javax.imageio.stream.ImageOutputStream;
 import javax.swing.SwingWorker;
 
 import com.sun.media.imageio.plugins.tiff.TIFFImageWriteParam;
 import com.sun.media.imageioimpl.plugins.tiff.TIFFImageWriter;
 import com.sun.media.imageioimpl.plugins.tiff.TIFFImageWriterSpi;
-import com.sun.media.jai.codec.ImageCodec;
-import com.sun.media.jai.codec.ImageEncoder;
-import com.sun.media.jai.codec.SeekableOutputStream;
-import com.sun.media.jai.codec.TIFFEncodeParam;
-import com.sun.media.jai.codec.TIFFField;
+
+
+
 
 public class CortaTiff extends SwingWorker<Void, Void> {
 
@@ -37,6 +39,7 @@ public class CortaTiff extends SwingWorker<Void, Void> {
 	private String P1x2;
 	private String P1y2;
 	private int imagenSalida;
+	
 
 public CortaTiff(String RutaOrigen, String nombreimagenIN, String P1x1,String P1y1, String P1x2, String P1y2, int imagenSalida){
 
@@ -51,7 +54,9 @@ public CortaTiff(String RutaOrigen, String nombreimagenIN, String P1x1,String P1
 
 private void cortatiff(File f) throws IOException, Exception {
 
-		try {
+	
+	
+	try {
 
 			//Falta depurar si el fichero de entrada es TIFF o JPEG
 			//Falta depurar si el fichero tiene que ser cortado como P1 y P2 o solamente P1
@@ -59,13 +64,15 @@ private void cortatiff(File f) throws IOException, Exception {
 			
 			
 			BufferedImage buffimage = ImageIO.read(f);
+			
 			final BufferedImage croppedimage = buffimage.getSubimage(Integer.parseInt(P1x1), Integer.parseInt(P1y1),Integer.parseInt(P1x2), Integer.parseInt(P1y2));
 			
 			int width = croppedimage.getWidth();											//Obtenemos las dimensiones X e Y de la imagen
 			int heigth = croppedimage.getHeight();
 			
-			//Falta obtener el COLOR SPACE de la imagen de entrada
 			
+			
+			/*
 			long fechams = f.lastModified();												//Obtenemos la fecha de creacion de la imagen de entrada
 			Date d = new Date(fechams);
 			Calendar c = new GregorianCalendar(); 
@@ -92,9 +99,11 @@ private void cortatiff(File f) throws IOException, Exception {
 			if (segundo.length()<2){
 				segundo = "0"+segundo;
 			}
+			
+			
 			String FECHACREACION_VAR = (annio + ":" + mes + ":" + dia + " " + hora + ":" + minuto + ":" + segundo);
 			
-			TIFFEncodeParam param = new TIFFEncodeParam();
+			
 			final int NEWSUBFILETYPE_TAG = 254;
 			final int IMAGEWIDTH_TAG = 256;
 			final int IMAGELENGTH_TAG = 257;
@@ -116,22 +125,139 @@ private void cortatiff(File f) throws IOException, Exception {
 			final int COPYRIGHT_TAG = 33432;
 			
 			
-			// PRUEBAS LEER METADATOS EXIF DEL FICHERO DE ENTRADA USANDO LA CLASE TIFFMETADATAREAD.JAVA DEBERÍA DEVOLVERNOS UN HASHMAP
-			
-			HashMap <String, String> hashmeta = TiffMetadataRead.LeerExif(f.getAbsolutePath().toString());
-
 			String DESCRIPTION_VAR = "Descripcion de la imagen tal cual aparece";
-			String MAKE_VAR = "Descripcion de la imagen tal cual aparece";
-			String MODEL_VAR = "Pues es mio";
-			String SOFTWARE_VAR = "Descripcion de la imagen tal cual aparece";
-			String ARTIST_VAR = "Tu si que eres un artista";
-			String COPYRIGHT_VAR = "Pues es mio";
+						String MAKE_VAR = "Descripcion de la imagen tal cual aparece";
+						String MODEL_VAR = "Pues es mio";
+						String SOFTWARE_VAR = "Descripcion de la imagen tal cual aparece";
+						String ARTIST_VAR = "Tu si que eres un artista";
+						String COPYRIGHT_VAR = "Pues es mio";
+			
+			*/
+			
+			// PRUEBAS LEER METADATOS EXIF DEL FICHERO DE ENTRADA USANDO LA CLASE TIFFMETADATAREAD.JAVA DEBERIA DEVOLVERNOS UN HASHMAP
+			
+			/* HashMap <String, String> hashmeta = TiffMetadataRead.LeerExif(f.getAbsolutePath().toString());
+
+			String DESCRIPTION_VAR = hashmeta.get("0x010E");
+			if (DESCRIPTION_VAR == null){
+				DESCRIPTION_VAR = " ";
+			}
+			if (DESCRIPTION_VAR.trim().length()%2 !=0){
+				//DESCRIPTION_VAR+="";
+				
+			}int cuentadigitos = DESCRIPTION_VAR.length();
+			String MAKE_VAR = hashmeta.get("0x010f");
+			
+			if (MAKE_VAR == null){
+				MAKE_VAR = " ";
+			}
+			MAKE_VAR+=" ";
+			
+			
+			String MODEL_VAR = hashmeta.get("0x0110");
+			if (MODEL_VAR == null){
+				MODEL_VAR = " ";
+			}
+			
+			
+			
+			String SOFTWARE_VAR = hashmeta.get("0x0131");
+			if (SOFTWARE_VAR == null){
+				SOFTWARE_VAR = " ";
+			}
+			if (SOFTWARE_VAR.trim().length()%2 !=0){
+				SOFTWARE_VAR += " ";
+			}cuentadigitos += SOFTWARE_VAR.length();
+			
+			String ARTIST_VAR = hashmeta.get("0x013B");
+			if (ARTIST_VAR == null){
+				ARTIST_VAR = " ";
+			}
+			
+			
+			String []COPYRIGHT_VAR = {hashmeta.get("0x8298")};
+			if (COPYRIGHT_VAR[0] == null){
+				COPYRIGHT_VAR[0] = " ";
+			}
+			
+			
+			System.out.println(cuentadigitos);
+			if (cuentadigitos%2 != 0){
+				
+				MODEL_VAR += " " + "\0";
+				
+			}
+			
+			*/
+			
+            
+			
+			
+			/*
+            BaselineTIFFTagSet base = BaselineTIFFTagSet.getInstance();
+     		TIFFTag tagNewSubfileType = base.getTag(BaselineTIFFTagSet.NEW_SUBFILE_TYPE_SINGLE_PAGE);
+     		TIFFTag tagMake = base.getTag(BaselineTIFFTagSet.TAG_MAKE);
+     		
+     		
+     		TIFFField make = new TIFFField (tagMake, TIFFTag.TIFF_ASCII,1, (Object) new String[] { MODEL_VAR });
+			*/
+			
+			
+			//IIORegistry registry = IIORegistry.getDefaultInstance();
+			 
+	        
+	        Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("tif");
+	        ImageReader reader = null;
+	        while (readers.hasNext())
+	        {
+	            reader = readers.next();
+	            
+	        }
+	 
+	       ImageInputStream stream = null;
+	        stream = ImageIO.createImageInputStream(f);
+	        reader.setInput(stream);
+			
+	        IIOMetadata imageMetadata = reader.getImageMetadata(0);
+	        //IIOMetadata streamMetadata = reader.getStreamMetadata();
+	        
+	        
+			ImageWriterSpi tiffspi = new TIFFImageWriterSpi();							
+			
+			TIFFImageWriter tiffwriter = (TIFFImageWriter) tiffspi.createWriterInstance();
+			
+			TIFFImageWriteParam tifparam = new TIFFImageWriteParam(Locale.US);
+			
+			tifparam.setCompressionMode(TIFFImageWriteParam.MODE_DISABLED);
+			tifparam.setDestinationOffset((new Point(0, 0)) );
+			tifparam.setTilingMode(TIFFImageWriteParam.MODE_DISABLED);
+			tifparam.setSourceSubsampling(1, 1, 0, 0);
+			
+			
+			File fOutputFile = new File(RutaOrigen + imagenSalida + ".tif");
+			OutputStream fos = new BufferedOutputStream(new FileOutputStream(fOutputFile));
+			ImageOutputStream ios = ImageIO.createImageOutputStream(fos);
+			ios.setBitOffset(0);
+			ios.setByteOrder(ByteOrder.LITTLE_ENDIAN);
+			
+			
+			tiffwriter.setOutput(ios);
+			tiffwriter.write(null, new IIOImage(croppedimage, null, imageMetadata), tifparam);
+			ios.flush();
+			tiffwriter.dispose();
+			ios.close();
+			
+			
+
+			/* TIFFEncodeParam param = new TIFFEncodeParam();
 			
 			param.setCompression(TIFFEncodeParam.COMPRESSION_NONE);						// Componemos la cabecera usando los TAGS definidos en las variables anteriores
 			param.setLittleEndian(true);
 			param.setWriteTiled(false);
 			param.setTileSize(0, 0);
-			TIFFField newsubfiletype = new TIFFField(NEWSUBFILETYPE_TAG,TIFFField.TIFF_LONG, 1, (Object) new long[] { 0 });
+			
+			
+			TIFFField newsubfiletype = new TIFFField (NEWSUBFILETYPE_TAG, TIFFTag.TIFF_LONG,1, (Object) new long[] { 0 });
 			TIFFField imagewidth = new TIFFField(IMAGEWIDTH_TAG,TIFFField.TIFF_LONG, 1, (Object) new long[] { width });
 			TIFFField imagelength = new TIFFField(IMAGELENGTH_TAG,TIFFField.TIFF_LONG, 1, (Object) new long[] { heigth });
 			TIFFField fillorder = new TIFFField(FILLORDER_TAG,TIFFField.TIFF_SHORT, 1, (Object) new char[] { 1 });
@@ -140,7 +266,7 @@ private void cortatiff(File f) throws IOException, Exception {
 			TIFFField model = new TIFFField(MODEL,TIFFField.TIFF_ASCII, 1, (Object) new String[] {MODEL_VAR});
 			TIFFField stripoffsets = new TIFFField(STRIPOFFSETS, TIFFField.TIFF_LONG, 1, (Object) new long[] {});
 			TIFFField orientation = new TIFFField(ORIENTATION_TAG,TIFFField.TIFF_SHORT, 1, (Object) new char[] { 1 });
-			TIFFField rowsperstrip = new TIFFField(ROWSPERSTRIP, TIFFField.TIFF_LONG,1,(Object) new long[]{});
+			TIFFField rowsperstrip = new TIFFField(ROWSPERSTRIP, TIFFField.TIFF_LONG,1,(Object) new long[]{heigth});
 			TIFFField stripsbytecounts = new TIFFField(STRIPSBYTECOUNTS, TIFFField.TIFF_LONG,1,(Object) new long[] {});
 			TIFFField xRes = new TIFFField(XRES_TAG, TIFFField.TIFF_RATIONAL,1, new long[][] { { (long) 300, 1 } });
 			TIFFField yRes = new TIFFField(YRES_TAG, TIFFField.TIFF_RATIONAL,1, new long[][] { { (long) 300, 1 } });
@@ -149,39 +275,35 @@ private void cortatiff(File f) throws IOException, Exception {
 			TIFFField software = new TIFFField(SOFTWARE,TIFFField.TIFF_ASCII, 1, (Object) new String[] {SOFTWARE_VAR});
 			TIFFField fecha = new TIFFField(FECHACREACION, TIFFField.TIFF_ASCII, 1, (Object) new String[] {FECHACREACION_VAR});
 			TIFFField artist = new TIFFField(ARTIST,TIFFField.TIFF_ASCII, 1, (Object) new String[] {ARTIST_VAR});
-			TIFFField copyright = new TIFFField(COPYRIGHT_TAG,TIFFField.TIFF_ASCII, 1, (Object) new String[] {COPYRIGHT_VAR});
+			TIFFField copyright = new TIFFField(COPYRIGHT_TAG,TIFFField.TIFF_ASCII, 1, (Object) new String[] {COPYRIGHT_VAR[0]});
 			
-			param.setExtraFields(new TIFFField[] { newsubfiletype, imagewidth, imagelength, fillorder, image_description, make, model, stripoffsets, orientation,
-					  rowsperstrip, stripsbytecounts, xRes, yRes,  planarconfiguration, unit_Inch, software, fecha, artist, copyright});
-
-            ImageWriterSpi tiffspi = new TIFFImageWriterSpi();							// Comenzamos a crear el fichero de salida
-			TIFFImageWriter writer = (TIFFImageWriter) tiffspi.createWriterInstance();
-
-			TIFFImageWriteParam tifparam = new TIFFImageWriteParam(Locale.US);
-			tifparam.setCompressionMode(1);
-
-			File fOutputFile = new File(RutaOrigen + imagenSalida + ".tif");
-			OutputStream fos = new BufferedOutputStream(new FileOutputStream(fOutputFile));
-			ImageOutputStream ios = ImageIO.createImageOutputStream(fos);
-			writer.setOutput(ios);
-			writer.write(null, new IIOImage(croppedimage, null, null), tifparam);
-			ios.flush();
-			writer.dispose();
-			ios.close();
-
-			File finishFile = new File(RutaOrigen + imagenSalida + ".tif");				// Escribimos la cabecera del TIFF con los EXIF creados anteriormente
-			OutputStream out = null;
-			out = new SeekableOutputStream(new RandomAccessFile(finishFile, "rw"));
+			param.setExtraFields(new TIFFField[] {newsubfiletype, imagewidth, imagelength, fillorder, image_description, make, model, orientation, xRes, yRes, planarconfiguration
+					,unit_Inch, software, fecha, artist, copyright});
+			
+			
+			File finishFile = new File(RutaOrigen + imagenSalida + "_"+  ".tif");				// Escribimos la cabecera del TIFF con los EXIF creados anteriormente
+			OutputStream os = new FileOutputStream(finishFile);
+			
 			BufferedImage iim = ImageIO.read(new File(RutaOrigen + imagenSalida + ".tif"));
-			ImageEncoder encoder = ImageCodec.createImageEncoder("tiff", out, param);
 			
-			encoder.encode(iim);
-			out.flush();
-			out.close();																// for security reasons null the output stream and call the garbage // collector; otherwise the JVM could still hold some open file locks
-			out = null;
+			
+			//out = new SeekableOutputStream(new RandomAccessFile(finishFile, "rw"));
+			
+			
+			//ImageEncoder encoder = ImageCodec.createImageEncoder("tiff", out, param);
+			TIFFImageEncoder encod = new TIFFImageEncoder(os,param);
+			
+			encod.encode(iim);
+			
+			//encoder.encode(iim);
+			os.flush();
+			os.close();																// for security reasons null the output stream and call the garbage // collector; otherwise the JVM could still hold some open file locks
+			os = null;
 			System.gc();
-
-			} catch (Exception e) {
+			
+			*/
+			
+		} catch (Exception e) {
 				System.out.println(e);
 			} finally {
 
@@ -191,7 +313,6 @@ private void cortatiff(File f) throws IOException, Exception {
 protected Void doInBackground() throws Exception {
 
 	cortatiff(new File(RutaOrigen + nombreimagenIN));
-	//System.out.println("Proceso TERMINADO");
 	return null;
 
 }
--- TAGer/src/App/TiffMetadataRead.java
+++ TAGer/src/App/TiffMetadataRead.java
@@ -14,42 +14,40 @@ import com.drew.metadata.exif.ExifIFD0Directory;
 																									//Esta clase nos tiene que devolver un objeto HasMap con los Exif del fichero que le pasemos
 
 public class TiffMetadataRead {
-	static HashMap<String, String> etiqueta;
+	
+	static ExifIFD0Directory exifdirectory;
 	
 	
 	
-public static HashMap <String, String> LeerExif (String filewcb) throws IOException{
+	
+public static ExifIFD0Directory LeerExif (String filewcb) throws IOException{
 		
-		etiqueta=null;
+		exifdirectory=null;
 		getExifFile(new File(filewcb));
-		return etiqueta;
+		return exifdirectory;
+		
 		
 	}
 
-static HashMap <String, String> getExifFile (File ficheroTiffWCB) throws IOException
+static ExifIFD0Directory getExifFile (File ficheroTiffWCB) throws IOException
 {
-	HashMap<String,String> hmtiff = new HashMap<String, String>();
+	
 	Metadata tiffMetadata = TiffMetadataReader.readMetadata(ficheroTiffWCB);							//Creamos objeto Metadata del fichero ficheroTiffWCB	
 	if (ficheroTiffWCB.toString().endsWith(".tif"))													//Comprobamos que es un .tif
 	{
 		ExifIFD0Directory exifdirectory = tiffMetadata.getDirectory(ExifIFD0Directory.class);		//Creamos el objeto exifdirectory del objeto tiffMetadata
 			if( exifdirectory != null )																//Comprobamos que no sea null
 			{
-				for(Iterator<Tag> i = exifdirectory.getTags().iterator(); i.hasNext(); )			//Iteramos exifdirectory
-				{
-				Tag tag = ( Tag )i.next();
-            	//System.out.println( "\t" + tag.getTagTypeHex() + " = " + tag.getDescription());
-            	hmtiff.put(tag.getTagTypeHex(),tag.getDescription());								//Vamos llenando el HashMap con el contenido de exifdirectory
-            	}
+				
+			} else{
+				
+				System.out.println( "El fichero " + ficheroTiffWCB + " no contiene datos Exif" );	
 			}
-			else
-			{
-			System.out.println( "El fichero " + ficheroTiffWCB + " no contiene datos Exif" );		//Si estaba null avisamos
-			}
+			
 	}else{
 		System.out.println("El fichero "+ ficheroTiffWCB + " no es del tipo TIFF");					//Si no era .tif avisamos
 		}
-		etiqueta = new HashMap<String, String> (hmtiff);
-		return etiqueta;
+		
+	return exifdirectory;
 }
 }
