f837fd173b43b8f6a63b50277d360b79276a71b8
franchi <franchi@JavierF.vinfra.local>
2015-01-28 18:57:04 +0100

    28/01/2015. Last Update. TIFF Write OK. Jhove Validation OK. Metadata from image_input copy to image_output. Working Hard.

--- TAGer/src/App/MetadataXMLProcess.java
+++ TAGer/src/App/MetadataXMLProcess.java
@@ -1,6 +1,7 @@
 package App;
 
 import java.io.File;
+import java.util.HashMap;
 
 import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.metadata.IIOMetadataNode;
@@ -19,14 +20,14 @@ public class MetadataXMLProcess {
 
 	
 	
-public static IIOMetadata metadatachange (IIOMetadata imageMetadata){
+public static IIOMetadata metadatachange (IIOMetadata imageMetadata, HashMap tablaExif){
 	
-	metaexif(imageMetadata);
+	metaexif(imageMetadata, tablaExif);
 	return imageMetadata;
-	
 }
 
-static void metaexif (IIOMetadata imageMetadata){
+
+static void metaexif (IIOMetadata imageMetadata, HashMap tablaExif){
 	
 	String formatonombres = imageMetadata.getNativeMetadataFormatName();
     IIOMetadataNode tiffRootNode = (IIOMetadataNode) imageMetadata.getAsTree(formatonombres);
@@ -37,36 +38,33 @@ static void metaexif (IIOMetadata imageMetadata){
     
    for(int i=0;i<miLista.getLength();i++){	    	   
 	   if (miLista.item(i).getAttributes().getNamedItem("number").getNodeValue().equals("272")){
-		   
 		   miLista.item(i).getChildNodes().item(0).getChildNodes().item(0).getAttributes().item(0).setNodeValue("Maquinita");
-		   
-	   }else{
+		}else{
 		   System.out.println("+++" + miLista.item(i).getAttributes().getNamedItem("number").getNodeValue());
 	   }
    }
     
-	
-    Transformer transformer = null;
-	try {
+   Transformer transformer = null;
+   try {
 		transformer = TransformerFactory.newInstance().newTransformer();
-	} catch (TransformerConfigurationException e1) {
+   } catch (TransformerConfigurationException e1) {
+		// TODO Bloque catch generado automáticamente
+	e1.printStackTrace();
+   } catch (TransformerFactoryConfigurationError e1) {
 		// TODO Bloque catch generado automáticamente
 		e1.printStackTrace();
-	} catch (TransformerFactoryConfigurationError e1) {
-		// TODO Bloque catch generado automáticamente
-		e1.printStackTrace();
-	}
-	transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-	DOMSource source = new DOMSource(tiffRootNode);
-	StreamResult console = new StreamResult(new File("salida.xml"));
-	try {
-		transformer.transform(source, console);
-	} catch (TransformerException e) {
-		// TODO Bloque catch generado automáticamente
-		e.printStackTrace();
-	}
+   }
+   transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+   DOMSource source = new DOMSource(tiffRootNode);
+   StreamResult console = new StreamResult(new File("salida.xml"));
+   try {
+	   transformer.transform(source, console);
+   } catch (TransformerException e) {
+		// TODO Bloque catch generado automáticamente	
+	   e.printStackTrace();
+   }
 	 
-	System.out.println("\nXML DOM Created Successfully..");
+   System.out.println("\nXML DOM Created Successfully..");
 	
 	
 }
